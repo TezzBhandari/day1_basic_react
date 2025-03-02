21ae2a8c843071975f5eefcece577fa4b6991eec
TezzBhandari <bhandaritezz18@gmail.com>
2025-03-02 11:47:43 +0545

    basic react day 1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+function App() {
+  return (
+    <div>
+      <h2 className="title">I am container</h2>
+      <p>hello world</p>
+      <Heading />
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Heading.tsx
@@ -0,0 +1,10 @@
+function Heading() {
+  return (
+    <div>
+      <h2>first</h2>
+      <h2>second</h2>
+    </div>
+  );
+}
+
+export default Heading;
