ff511ddbc5bf4f3705888fe24c30a81266b61b84
TezzBhandari <bhandaritezz18@gmail.com>
2025-03-06 10:51:41 +0545

    list rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
-import Avatar, { Person } from "./components/Avatar";
 import Profile from "./components/Profile";
+interface Profile {
+  id: number;
+  profileImg: string;
+  size: number;
+}
+const profiles: Array<Profile> = [
+  {
+    id: 1,
+    profileImg:
+      "https://images.pexels.com/photos/30495756/pexels-photo-30495756/free-photo-of-rustic-wooden-chairs-in-lush-antalya-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    size: 100,
+  },
+  {
+    id: 2,
+    profileImg: "https://i.imgur.com/1bX5QH6.jpg",
+    size: 100,
+  },
+  {
+    id: 3,
+    profileImg: "https://i.imgur.com/OKS67lh.jpg",
+    size: 100,
+  },
+  {
+    id: 4,
+    profileImg:
+      "https://images.pexels.com/photos/30495756/pexels-photo-30495756/free-photo-of-rustic-wooden-chairs-in-lush-antalya-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    size: 60,
+  },
+];
 
 function App() {
-  const sam: Person = {
-    name: "sam",
-    profile: "https://i.imgur.com/1bX5QH6.jpg",
-  };
-
-  const rita: Person = {
-    name: "rita",
-    profile: "https://i.imgur.com/OKS67lh.jpg",
-  };
-
   return (
-    <>
-      <Profile imageUrl="https://i.imgur.com/1bX5QH6.jpg"  size={50}/>
-      <Profile imageUrl="https://i.imgur.com/OKS67lh.jpg" size={100} />
-    </>
+    <div>
+      {profiles.map((profile) => {
+        return (
+          <Profile
+            key={profile.id}
+            imageUrl={profile.profileImg}
+            size={profile.size}
+          />
+        );
+      })}
+    </div>
   );
 }
 
